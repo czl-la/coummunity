adb4a6c176fc4dec9a21c3a673530081d534909b
czl <1312615230@qq.com>
2019-09-04 17:31:14 +0800

    实现登录

--- src/main/java/com/example/demo/controller/AuthorizeController.java
+++ src/main/java/com/example/demo/controller/AuthorizeController.java
@@ -9,6 +9,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 public class AuthorizeController {
     @Autowired
@@ -22,7 +24,8 @@ public class AuthorizeController {
     private String redirectUri;
     @GetMapping("/callback")
     public String callback(@RequestParam(name="code")String code,
-                           @RequestParam(name="state")String state){
+                           @RequestParam(name="state")String state,
+                           HttpServletRequest request){
         AccessTokenDto accessTokenDto=new AccessTokenDto();
         accessTokenDto.setClient_id(clientId);
         accessTokenDto.setClient_secret(clientSecret);
@@ -31,7 +34,12 @@ public class AuthorizeController {
         accessTokenDto.setState(state);
         String accessToken=githubProvider.getAccessToken(accessTokenDto);
         GithubUserDto user=githubProvider.getUser(accessToken);
-        System.out.println(user.getName());
-        return "index";
+        if(user!=null){
+            request.getSession().setAttribute("user",user);
+            return "redirect:/";
+        }else{
+            return "redirect:/";
+        }
+
     }
 }
--- src/main/java/com/example/demo/dto/GithubUserDto.java
+++ src/main/java/com/example/demo/dto/GithubUserDto.java
@@ -1,8 +1,10 @@
 package com.example.demo.dto;
 
 import lombok.Data;
+import lombok.ToString;
 
 @Data
+@ToString
 public class GithubUserDto {
     private String name;
     private String bio;
